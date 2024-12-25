8a03ea2957a4067a645ea37fb34f994841e0975c
enzoboto1 <enzoboto1.0@gmail.com>
2024-12-25 19:11:44 -0300

    implementando metodo de create

--- microservicos/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ microservicos/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -1,22 +1,52 @@
 package com.eazybytes.accounts.service.impl;
 
+import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.exceptions.CustomerAlreadyExistsException;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.LocaleResolver;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
 
 @Service
 @AllArgsConstructor
 public class AccountsServiceImpl implements IAccountsService {
 
+    private final LocaleResolver localeResolver;
     private AccountsRepository accountsRepository;
     private CustomerRepository customerRepository;
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Optional<Customer> optionalCustomer = customerRepository.findByMobileNumber(customer.getMobileNumber());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Já existe um usuário com esse número de telefone "
+                    + customerDto.getMobileNumber());
+        }
+        Customer savedCostumer = customerRepository.save(customer);
+        accountsRepository.save(createNewAccount(savedCostumer));
+    }
 
+    private Accounts createNewAccount(Customer custumer) {
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(custumer.getCustomerId());
+        newAccount.setAccountNumber(1000000000L + new Random().nextInt(900000000));
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+        newAccount.setCreatedBy("Anonymous");
+        newAccount.setCreatedAt(LocalDateTime.now());
+
+        return newAccount;
     }
 
 }
