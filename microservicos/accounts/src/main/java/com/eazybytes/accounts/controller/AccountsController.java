2a0df86619f1a701f2a71ccedf7af50a56ff94dd
enzoboto1 <enzoboto1.0@gmail.com>
2025-01-31 00:00:23 -0300

    removendo construtor do controller ´para teste de erro

--- microservicos/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ microservicos/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
